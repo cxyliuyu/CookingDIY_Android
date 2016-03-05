62b2ac0e90237a3901d17c9d17baa30c80d08ab8
ly <yu948324577yu>
2016-03-05 11:13:30 +0800

    常规提交

--- app/src/main/java/com/cxyliuyu/www/cookingdiy_android/MainActivity.java
+++ app/src/main/java/com/cxyliuyu/www/cookingdiy_android/MainActivity.java
@@ -1,5 +1,6 @@
 package com.cxyliuyu.www.cookingdiy_android;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -41,6 +42,8 @@ public class MainActivity extends AppCompatActivity
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
+
+        Intent intent = new Intent();
     }
 
     @Override
