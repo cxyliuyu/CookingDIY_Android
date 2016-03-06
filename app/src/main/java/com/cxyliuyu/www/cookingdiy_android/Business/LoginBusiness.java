65e0745c4a1001a066b103b9d6247739f6d49c6d
ly <yu948324577yu>
2016-03-07 00:00:07 +0800

    常规提交

---
+++ app/src/main/java/com/cxyliuyu/www/cookingdiy_android/Business/LoginBusiness.java
@@ -0,0 +1,17 @@
+package com.cxyliuyu.www.cookingdiy_android.Business;
+
+import android.os.Handler;
+
+/**
+ * Created by ly on 2016/3/6.
+ */
+public class LoginBusiness {
+
+    //登录的业务逻辑
+    public void login(final Handler loginHandler){
+
+    }
+    public void logout(final Handler logoutHandler){
+
+    }
+}
