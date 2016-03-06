ed0e9b554b336dc889979b95f52d8a406e21c8c8
ly <yu948324577yu>
2016-03-07 00:00:18 +0800

    常规提交

---
+++ app/src/main/java/com/cxyliuyu/www/cookingdiy_android/utils/URLUtils.java
@@ -0,0 +1,9 @@
+package com.cxyliuyu.www.cookingdiy_android.utils;
+
+/**
+ * Created by ly on 2016/3/6.
+ */
+public class URLUtils {
+    public static String loginURL = "";
+    public static String key = "";
+}
