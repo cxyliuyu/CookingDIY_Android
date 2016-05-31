057d8bfb6572f60d0970c3eab010407ed6da625e
cxyliuyu <cxyliuyu@sina.com>
2016-06-01 00:39:02 +0800

    改用Volley框架

---
+++ app/src/main/java/com/cxyliuyu/www/cookingdiy_android/utils/VolleyRequest.java
@@ -0,0 +1,35 @@
+package com.cxyliuyu.www.cookingdiy_android.utils;
+
+import android.content.Context;
+
+import com.android.volley.AuthFailureError;
+import com.android.volley.Request;
+import com.android.volley.toolbox.StringRequest;
+import com.cxyliuyu.www.cookingdiy_android.MyApplication;
+
+import java.lang.reflect.Method;
+import java.util.Map;
+
+/**
+ * Created by ly on 2016/5/31.
+ */
+public class VolleyRequest {
+
+    public static Context context;
+
+    public static void RequestGet(Context context,String url,String tag,VolleyListener volleyListener){
+        StringRequest stringRequest = new StringRequest(Request.Method.GET,url,volleyListener.responseListener(),
+                volleyListener.errorListener());
+        stringRequest.setTag(tag);
+        MyApplication.getVolleyRequestQueue().add(stringRequest);
+    }
+    public static void RequestPost(Context context,String url,String tag,VolleyListener volleyListener,final Map<String,String>map){
+        StringRequest stringRequest = new StringRequest(Request.Method.POST,url,
+                volleyListener.responseListener(),volleyListener.errorListener()){
+            @Override
+            protected Map<String, String> getParams() throws AuthFailureError {
+                return map;
+            }
+        };
+    }
+}
